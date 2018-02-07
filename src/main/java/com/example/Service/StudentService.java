6fb1d8921293e202f9e97e349c1487cd518b996a
Szymon <szymonsadowski3@gmail.com>
2018-02-07 21:23:06 +0100

    Initial commit

---
+++ src/main/java/com/example/Service/StudentService.java
@@ -0,0 +1,35 @@
+package com.example.Service;
+
+import com.example.Dao.StudentDao;
+import com.example.Entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentDao studentDao;
+
+    public Collection<Student> getAllStudents() {
+        return studentDao.getAllStudents();
+    }
+
+    public Student getStudentById(int id) {
+        return studentDao.getStudentById(id);
+    }
+
+    public void removeStudentById(int id) {
+        this.studentDao.removeStudentById(id);
+    }
+
+    public void updateStudent(Student student) {
+        studentDao.updateStudent(student);
+    }
+
+    public void insertStudent(Student student) {
+        studentDao.insertStudentToDb(student);
+    }
+}
